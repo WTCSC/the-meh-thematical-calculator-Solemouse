e408931e7b067833d34bd483805e5ebac1c0ea06
Solemouse <jcdreier2009@gmail.com>
2025-10-24 12:20:05 -0600

    add and fix tests

--- test_calculator.py
+++ test_calculator.py
@@ -1,5 +1,5 @@
 import pytest
-from MehCalculator.py import add, subtract, multiply, divide, exponent
+from MehCalculator import add, subtract, multiply, divide, exponent
 
 def test_add_two_pos():
     assert add(8, 3) == 11
@@ -23,7 +23,7 @@ def test_multiply_two_pos():
 def test_multiply_two_two_digit_pos():
     assert multiply(87, 35) == 3045
 def test_multiply_two_decimal():
-    assert multiply(6.75, 3.93) == 26.52
+    assert multiply(6.75, 3.93) == 26.5275
 def test_multiply_by_zero():
     assert multiply(15, 0) == 0
 
@@ -36,11 +36,11 @@ def test_divide_by_two_pos():
 def test_divide_by_one():
     assert divide(8, 1) == 8
 def test_divide_by_zero():
-    assert divide(8, 0) == None
+    assert divide(8, 0) == False
 
 def test_power_one():
     assert exponent(5, 1) == 5
 def test_power_two():
-    assert exponent(5, 5) == 25
+    assert exponent(5, 5) == 3125
 def test_power_zero():
     assert exponent(5, 0) == 1
