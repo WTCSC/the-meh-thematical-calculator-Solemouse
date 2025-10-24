d771daa2afa1d23aee1d80d28f2eab8b43daefc2
Solemouse <jcdreier2009@gmail.com>
2025-10-23 21:31:58 -0600

    Finish writing tests for calculator

--- Calculatortest.py
+++ Calculatortest.py
@@ -14,3 +14,33 @@ def test_subtract_two_two_digit_pos():
     assert subtract(87, 35) == 52
 def test_subtract_two_decimal():
     assert subtract(6.75, 3.93) == 2.82
+def test_subtract_lead_to_negative():
+    assert subtract(175, 225) == -50
+
+
+def test_multiply_two_pos():
+    assert multiply(8, 3) == 24
+def test_multiply_two_two_digit_pos():
+    assert multiply(87, 35) == 3045
+def test_multiply_two_decimal():
+    assert multiply(6.75, 3.93) == 26.52
+def test_multiply_by_zero():
+    assert multiply(15, 0) == 0
+
+def test_divide_by_two_pos():
+    assert divide(8, 5) == 1.6
+def test_divide_by_two_two_digit_pos():
+    assert divide(75, 30) == 2.5
+def test_divide_by_two_pos():
+    assert divide(8, 5) == 1.6
+def test_divide_by_one():
+    assert divide(8, 1) == 8
+def test_divide_by_zero():
+    assert divide(8, 0) == None
+
+def test_power_one():
+    assert exponent(5, 1) == 5
+def test_power_two():
+    assert exponent(5, 5) == 25
+def test_power_zero():
+    assert exponent(5, 0) == 1
